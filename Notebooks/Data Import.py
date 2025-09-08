03a192aa678bb421151c62bf44fa210844d7c4d2
Utkarsh2506 <utkarshbundela13@gmail.com>
2025-09-09 01:06:01 +0530

    Update Data Import.py

--- Notebooks/Data Import.py
+++ Notebooks/Data Import.py
@@ -3,16 +3,17 @@ from sqlalchemy import create_engine
 
 # Replace 'your_password' with your actual MySQL root password.
 db_user = 'root'
-db_password = 'Bundela0421'
+db_password = 'your_password'
 db_host = 'localhost'
 db_name = 'factory_db'
 
 # Replace 'Filepath_to_your/Factory Dataset.csv' with your actual file path to the dataset.
-df = pd.read_csv('C:/Users/user/OneDrive/Documents/Bunny/Projects/Smart Factory Analytics/Smart Factory/Data/Factory Dataset.csv')
+df = pd.read_csv('Filepath_to_your/Factory Dataset.csv')
 df = df.drop('UDI', axis=1)
 
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}')
 
 df.to_sql('machine_data', con=engine, if_exists='append', index=False)
+
 
 print("Data has been successfully loaded into the 'machine_data' table in MySQL.")
